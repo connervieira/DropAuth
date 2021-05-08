0bc68770aa4ac91d54a41e819239a26701c72e39
Conner Vieira <connervieira@tutanota.com>
2021-05-08 14:13:34 -0400

    Added way to configure redirects

---
+++ authentication.php
@@ -0,0 +1,10 @@
+<?
+session_start(); // Start a PHP session.
+if (isset($_SESSION['loggedin'])) { // Check to see if the user is already signed in.
+    $username = $_SESSION["username"];
+} else {
+    if ($force_login_redirect == true) {
+        header("Location: ./signin.php");
+    }
+}
+?>
